b6565798e8e7b7e30f472d525808049e074bf7b0
zhoulihuang <17801029594@163.com>
2022-06-18 16:52:14 +0800

    spring-boot-starter 初始化可运行项目

---
+++ program-skill/src/main/java/com/zm/web/program/skill/ProgramApplication.java
@@ -0,0 +1,24 @@
+/**
+ * 版权所有(C)，上海海鼎信息科技有限公司，2019~2022，所有权利保留。
+ * <p>
+ * 项目名：	web
+ * 文件名：	${file_name}
+ * 模块说明：
+ * 修改历史：
+ * 2022/6/18 - zhoulihuang - 创建。
+ */
+package com.zm.web.program.skill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhoulihuang
+ */
+@SpringBootApplication
+public class ProgramApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProgramApplication.class, args);
+  }
+}
